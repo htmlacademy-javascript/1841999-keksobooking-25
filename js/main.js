91b41b204925eb0078103295156decae11750d43
Eugeny3 <hello31323334@gmail.com>
2022-03-21 16:49:12 +0500

    Внесены исправления, добавлена функция с простыми числами

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-function getRandomArbitrary(min, max, num) {
-  if (min >= max) {
-    alert('Минимальное число не может быть больше максимального!');
-  }
-  return (Math.random() * (max - min) + min).toFixed(num);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getRandomArbitrary(2, 3, 2));
+console.log(getRandomInt(2, 4));
+
+function getRandomFloat(min, max, num) {
+  if (min >= max) {
+    throw new Error('Минимальное число не может быть больше максимального!');
+  }
+  return Number((Math.random() * (max - min) + min).toFixed(num));
+}
+
+console.log(getRandomFloat(2, 3, 2));
